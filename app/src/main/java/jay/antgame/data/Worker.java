1ebb918dc179e8bc878c26459dc3e410b64e7a23
ProfDrPfeiffer <yannick.pf@web.de>
2016-07-01 14:51:21 +0200

    Ameisen laufen, suchen Futterquellen und laufen dann zurücl zu nest

--- app/src/main/java/jay/antgame/data/Worker.java
+++ app/src/main/java/jay/antgame/data/Worker.java
@@ -10,7 +10,7 @@ public class Worker extends Ant {
 
     private double movementSpeed = 3;
 
-    private final double nearTargetVariable = 5;
+    private final double nearFoodVariable = 20;
     private final double randomRotation = 1;
 
     public Worker(Position position) {
@@ -19,17 +19,29 @@ public class Worker extends Ant {
 
     public void tick(World world){
 
+        //Wenn hat ein Ziel zulaufen auf Ziel
         if(targetPosition!=null){
-            double x = targetPosition.getX()-pos.getX();
-            double y = targetPosition.getY()-pos.getY();
-            double targetAngle = Math.tanh( y/x );
-            pos.addX( movementSpeed*Math.cos(targetAngle) );
-            pos.addY( movementSpeed*Math.sin(targetAngle) );
+
+            //Wenn Ziel noch nicht erreicht weiterlaufen
+            if(!checkIfOn(targetPosition, 10)) {
+                double x = targetPosition.getX() - pos.getX();
+                double y = targetPosition.getY() - pos.getY();
+                double targetAngle = Math.tanh(y / x);
+                pos.addX(movementSpeed * Math.cos(targetAngle));
+                pos.addY(movementSpeed * Math.sin(targetAngle));
+            }else{
+                //Wenn Ziel erreicht kein Ziel mehr
+                targetPosition = null;
+            }
+
         }else{
+            //Wenn kein Ziel Winkel zufällig ändern und weiterlaufen
             if(!checkIfOnFQ(world)) {
                 angle += (0.5 - Math.random()) * randomRotation;
                 pos.addX(movementSpeed * Math.cos(angle));
                 pos.addY(movementSpeed * Math.sin(angle));
+            }else{
+                targetPosition = world.getNest().getPosition();
             }
         }
 
@@ -38,15 +50,17 @@ public class Worker extends Ant {
     private boolean checkIfOnFQ(World world){
         boolean onFQ = false;
         for(FoodSource foodSource: world.getFoodSources()){
-            if(checkIfOn(foodSource.getPosition()))
+            if(checkIfOn(foodSource.getPosition(),nearFoodVariable))
                 onFQ = true;
         }
         return onFQ;
     }
 
-    private boolean checkIfOn(Position targetPosition){
+    private boolean checkIfOn(Position targetPosition, double nearTargetVariable){
 
-        if(targetPosition.getX()-pos.getX()<nearTargetVariable&&targetPosition.getY()-pos.getY()<nearTargetVariable)
+        boolean nearX = -nearTargetVariable<targetPosition.getX()-pos.getX()&&targetPosition.getX()-pos.getX()<nearTargetVariable;
+        boolean nearY = -nearTargetVariable<targetPosition.getY()-pos.getY()&&targetPosition.getY()-pos.getY()<nearTargetVariable;
+        if(nearX&&nearY)
             return true;
         else
             return false;
